e385fd40b62a794eca24741aad3ee2167f77c091
Suvo2025 <swapnadas4554@gmail.com>
2025-09-24 23:34:41 +0530

    Initial commit of updated the library management system

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";  // FastAPI backend
+const API_BASE = window.location.origin;  // FastAPI backend
 
 document.addEventListener("DOMContentLoaded", () => {
   // Initial loads
